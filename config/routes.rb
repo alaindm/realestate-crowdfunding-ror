d6d7960d2db79282b627c697731c2be9088f2f63
Alain <alaindemarchi@gmail.com>
2016-07-12 22:07:57 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/imobank', to: 'pages#imobank'
   get '/cadastrar', to: 'users#new'
-  
+  post '/signup',  to: 'users#create'
+  resources :users
 end
