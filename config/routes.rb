6a2b11f0c57eae2a794084ad0ef1958e7e531b4c
Alain <alaindemarchi@gmail.com>
2016-07-13 17:19:16 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/cadastrar', to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
