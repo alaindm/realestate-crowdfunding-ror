ff59fd6812a06ce07e12f9d4773cbd9efc935e05
Alain De Marchi <alaindemarchi@gmail.com>
2016-08-09 18:00:11 -0300

    Sendgrid setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Configure ActionMailer to Use SendGrid
+# source: https://sendgrid.com/docs/Integrate/Frameworks/rubyonrails.html
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'alaindm',
+  :password => 'fdas2020',
+  :domain => 'imobank.com.br',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
